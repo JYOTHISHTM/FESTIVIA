3f8df2ecf2ad970d6ed54e7b520ff81ed4a9685b
Jyothish T M <jothishtm2002@gmail.com>
2025-07-05 10:25:54 +0530

    maintance feature turn off

--- FRONTEND/src/config/config.ts
+++ FRONTEND/src/config/config.ts
@@ -1,7 +1,8 @@
 
 // const BASE_URL = 'http://localhost:5001';
 const BASE_URL='https://festivia-api.jothish.online'
-const MAINTENANCE_MODE = true; // 🔴 Site OFF | Set to false to turn it ON
+// const MAINTENANCE_MODE = true; 
+const MAINTENANCE_MODE = false; 
 
 
 export const API_CONFIG = {
